9d2a0d6ba74a4b237c4e8d3811985cb6b1855c67
harrisonpim <hello@harrisonpim.com>
2022-09-17 14:05:41 +0100

    future years' data should also be downloadable without modifying the script

--- tasks/get_data/main.py
+++ tasks/get_data/main.py
@@ -2,21 +2,21 @@ import httpx
 from pathlib import Path
 
 data_url = "https://mrv.emsa.europa.eu/api/public-emission-report/reporting-period-document/binary/"
-
-
-data_dir = Path("/data/").absolute()
+data_dir = Path("/data/raw").absolute()
 if not data_dir.exists():
-    data_dir.mkdir(parents=True)
+    data_dir.mkdir(parents=True, exist_ok=True)
 
-
-for year in range(2018, 2022):
-    file_path = data_dir / "raw" / f"{year}.xlsx"
-    if not file_path.exists():
-        print(f"Downloading {year}...")
-        Path(file_path).parent.mkdir(exist_ok=True, parents=True)
-        with open(file_path, "wb") as f:
-            url = data_url + str(year)
-            file_data = httpx.get(url).content
-            f.write(file_data)
-    else:
+year = 2018
+while True:
+    file_path = data_dir / f"{year}.xlsx"
+    if file_path.exists():
         print(f"{year} already exists.")
+    else:
+        response = httpx.get(data_url + str(year))
+        if response.status_code == 200:
+            print(f"Downloading data for {year}...")
+            with open(file_path, "wb") as f:
+                f.write(response.content)
+        else: 
+            break
+    year += 1
