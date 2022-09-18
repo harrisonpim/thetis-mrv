ad92dc840a7d3b151f203da2cd56b058386fb6db
harrisonpim <hello@harrisonpim.com>
2022-09-18 18:06:35 +0100

    format responses with orjson

--- api/app/main.py
+++ api/app/main.py
@@ -1,12 +1,18 @@
+import orjson
 import os
-from fastapi import FastAPI, HTTPException
-from src import get_db_engine, Entry
+from fastapi import FastAPI, HTTPException, responses
+from src import get_db_engine, Entry, Ship
 
 from sqlmodel import Session, select
-from typing import List
+from typing import List, Any
+
+class ORJSONResponse(responses.JSONResponse):
+    media_type = "application/json"
+    def render(self, content: Any) -> bytes:
+        return orjson.dumps(content)
 
 
-app = FastAPI()
+app = FastAPI(default_response_class=ORJSONResponse)
 
 engine = get_db_engine(
     user=os.environ["POSTGRES_USER"],
@@ -27,10 +33,22 @@ def read_entries():
         return [entries]
 
 
-@app.get("/entries/{entry_id}")
+@app.get("/entries/{entry_id}", response_model=Entry)
 def read_entry(entry_id: int):
     with Session(engine) as session:
         entry = session.get(Entry, entry_id)
         if not entry:
             raise HTTPException(status_code=404, detail="Entry not found")
         return entry
+
+
+@app.get("/ships/", response_model=List[Ship])
+def read_ships():
+    with Session(engine) as session:
+        return {}
+
+
+@app.get("/ships/{ship_id}", response_model=List[Ship])
+def read_ships():
+    with Session(engine) as session:
+        return {}
